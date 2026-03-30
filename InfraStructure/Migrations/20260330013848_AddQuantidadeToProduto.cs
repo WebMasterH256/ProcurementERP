98d3d6bc07a7d6deda4dd305090c17536405648c
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-03-29 22:45:33 -0300

    Snapshot desatualizada do banco de dados com a falta da coluna "Quantidade" na tabela "Produto" removida

---
+++ InfraStructure/Migrations/20260330013848_AddQuantidadeToProduto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraStructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuantidadeToProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantidade",
+                table: "Produto",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Produto");
+        }
+    }
+}
