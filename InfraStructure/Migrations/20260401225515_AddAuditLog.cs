ffe865f378a7eafe0ffde19e28368e8b71c1007d
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-05 23:03:13 -0300

    Snapshots atualizadas do EF que incluem a tabela "AuditLog"

---
+++ InfraStructure/Migrations/20260401225515_AddAuditLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraStructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: false),
+                    StatusAnterior = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    StatusNovo = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLog", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLog");
+        }
+    }
+}
