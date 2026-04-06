0a5a93841f3662e348dc7bacade5cdadc764b2aa
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-05 23:09:58 -0300

    Adicionada a classe "AuditLog" para registro de auditoria

---
+++ Domain/AuditLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain;
+
+public class AuditLog
+{
+	public int Id { get; set; }
+	public DateTime Data { get; set; }
+	public int PedidoId { get; set; }
+	public string StatusAnterior { get; set; }
+	public string StatusNovo { get; set; }
+	public int UsuarioId { get; set; }
+}
