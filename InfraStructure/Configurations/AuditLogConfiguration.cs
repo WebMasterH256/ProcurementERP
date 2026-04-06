e672610e30b3eac383a8cd475b0118c0c69cc30b
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-05 23:06:42 -0300

    Adicionada a "Configurations" da classe/tabela "AuditLog"

---
+++ InfraStructure/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain;
+
+namespace InfraStructure.Configurations;
+
+public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
+{
+	public void Configure(EntityTypeBuilder<AuditLog> builder)
+	{
+		builder.ToTable("AuditLog");
+		builder.HasKey(a => a.Id);
+		builder.Property(a => a.StatusAnterior)
+			.IsRequired()
+			.HasMaxLength(255);
+		builder.Property(a => a.StatusNovo)
+			.IsRequired()
+			.HasMaxLength(255);
+		builder.Property(a => a.Data)
+			.IsRequired();
+		builder.Property(a => a.PedidoId)
+			.IsRequired();
+		builder.Property(a => a.UsuarioId)
+			.IsRequired();
+	}
+}
