362300e225026dd820f61635d1e6a510ad514840
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-09 13:30:59 -0300

    Controller que contém o endpoint para o método que retorna o relatório

---
+++ API/Controllers/RelatorioController.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Controllers;
+
+using InfraStructure.Reports;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+[ApiController]
+[Route("relatorio")]
+public class RelatorioController : ControllerBase
+{
+	private readonly PedidoReportRepository _prr;
+
+	public RelatorioController(PedidoReportRepository prr) { _prr = prr; }
+
+	[HttpGet("pedidos")]
+	public async Task<IActionResult> GetRelatorioPedidos()
+	{
+		var relatorio = await _prr.GetRelatorioAsync();
+		return Ok(relatorio);
+	}
+}
