2819ca4c91a09a76d6ebdf22fc323af4592f190c
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-09 13:20:39 -0300

    Classe que contém características da tabela usada para um relatório

---
+++ Domain/RelatorioPedido.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain;
+
+public class RelatorioPedido
+{
+	public int PedidoId { get; set; }
+	public string Departamento { get; set; }
+	public string Usuario { get; set; }
+	public string Status { get; set; }
+	public decimal ValorTotal { get; set; }
+	public DateTime DataCriacao { get; set; }
+}
